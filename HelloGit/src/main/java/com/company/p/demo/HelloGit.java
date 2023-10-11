535cd5b42f32aacd7ca426d3b9bbad767f83be07
zhaowenming <312837814@qq.com>
2023-10-11 14:04:00 +0800

    我在DEV修复了一个BUG

--- HelloGit/src/main/java/com/company/p/demo/HelloGit.java
+++ HelloGit/src/main/java/com/company/p/demo/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("hello git. ");
         System.out.println("hello git111.");
         System.out.println("创建分支。");
+        System.out.println("我在DEV修复了一个BUG");
     }
 }
